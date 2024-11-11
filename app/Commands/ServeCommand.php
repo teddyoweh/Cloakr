aa583155ce38cb4cb7fc77ffb5f300000175f795
Cloakr <teddyoweh@gmail.com>
2024-11-11 07:30:34 +0200

    wip

--- app/Commands/ServeCommand.php
+++ app/Commands/ServeCommand.php
@@ -21,12 +21,18 @@ class ServeCommand extends Command
             $this->info("Cloakr server running on port ".$this->option('port').".");
         });
 
+        $validateAuthTokens = config('cloakr.server.validate_auth_tokens');
+
+        if ($this->option('validateAuthTokens')) {
+            $validateAuthTokens = true;
+        }
+
         (new Factory())
             ->setLoop($loop)
             ->setHost($this->argument('host'))
             ->setPort($this->option('port'))
             ->setHostname($this->argument('hostname'))
-            ->validateAuthTokens($this->option('validateAuthTokens'))
+            ->validateAuthTokens($validateAuthTokens)
             ->createServer()
             ->run();
     }

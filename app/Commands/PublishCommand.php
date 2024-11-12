06bfd59543585bba18b0a99d1faa9a9b87fecd48
Cloakr <teddyoweh@gmail.com>
2024-11-12 12:38:00 +0200

    wip

--- app/Commands/PublishCommand.php
+++ app/Commands/PublishCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class PublishCommand extends Command
 {
-    protected $signature = 'publish';
+    protected $signature = 'publish {--force}';
 
     protected $description = 'Publish the cloakr configuration file';
 
@@ -18,7 +18,7 @@ class PublishCommand extends Command
             'config.php'
         ]);
 
-        if (file_exists($configFile)) {
+        if (! $this->option('force') && file_exists($configFile)) {
             $this->error('Cloakr configuration file already exists at '.$configFile);
             return;
         }

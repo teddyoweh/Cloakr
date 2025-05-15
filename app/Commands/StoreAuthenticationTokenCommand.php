5af6e8779b53e507e52a860c42d9142ff7aa141a
Cloakr <teddyoweh@gmail.com>
2025-05-15 13:32:53 +0200

    Expose info command

--- app/Commands/StoreAuthenticationTokenCommand.php
+++ app/Commands/StoreAuthenticationTokenCommand.php
@@ -46,7 +46,7 @@ class StoreAuthenticationTokenCommand extends Command
 
         if (!$this->option('no-interaction')) {
 
-            render('<div class="ml-2"><div class="text-pink-500 font-bold"><span class="font-bold pr-0.5">></span> Cloakr</div>');
+            render('<div class="ml-2 text-pink-500 font-bold"><span class="pr-0.5">></span> Cloakr</div>');
             render("<div class='ml-3'>Setting up new Cloakr token <span class='font-bold'>$token</span>...</div>");
 
             (new SetupCloakrProToken)($token);

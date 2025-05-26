1a11773ace28cecb1c83c6e8b654c56cff37b9fc
Cloakr <teddyoweh@gmail.com>
2025-05-26 15:52:14 +0100

    Endpoint for single request log

--- app/Http/Controllers/GetLogController.php
+++ app/Http/Controllers/GetLogController.php
@@ -2,19 +2,29 @@
 
 namespace Cloakr\Client\Http\Controllers;
 
-use Cloakr\Client\Contracts\LogStorageContract;
+use Cloakr\Client\Http\HttpClient;
+use Cloakr\Client\Logger\RequestLogger;
 use Cloakr\Common\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Message;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Ratchet\ConnectionInterface;
 
 class GetLogController extends Controller
 {
-    public function __construct(protected LogStorageContract $logStorage)
+    public function __construct(protected RequestLogger $requestLogger, protected HttpClient $httpClient)
     {
     }
 
     public function handle(Request $request, ConnectionInterface $httpConnection)
     {
-        // TODO:
+        $loggedRequest = $this->requestLogger->findLoggedRequest($request->get('log'));
+
+        if (is_null($loggedRequest)) {
+            $httpConnection->send(Message::toString(new Response(404)));
+            return;
+        }
+
+        $httpConnection->send(respond_json($loggedRequest));
     }
 }
